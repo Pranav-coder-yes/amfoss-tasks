0736f6556cfdf9dcd2e29d12bfc52b1723777867
Pranav-coder-yes <mpranav0505@gmail.com>
2024-12-30 21:23:00 +0530

    task3

---
+++ task-3/index.js
@@ -0,0 +1,20 @@
+var w = new Audio();
+w.src="Sounds/crash.mp3";
+
+var a = new Audio();
+a.src="Sounds/kick-bass.mp3";
+
+var s = new Audio();
+s.src="Sounds/snare.mp3";
+
+var d = new Audio();
+d.src="Sounds/tom-1.mp3";
+
+var j = new Audio();
+j.src="Sounds/tom-2.mp3";
+
+var k = new Audio();
+k.src="Sounds/tom-3.mp3";
+
+var l = new Audio();
+l.src="Sounds/tom-4.mp3";
