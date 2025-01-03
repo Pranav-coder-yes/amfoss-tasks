ede33efd0974148e984878403ba28c9f7e1fbcf0
Pranav-coder-yes <mpranav0505@gmail.com>
2025-01-03 22:53:24 +0530

    Update pyt.py

--- task-2/pyt.py
+++ task-2/pyt.py
@@ -1,14 +1,12 @@
 import pytesseract
 from PIL import Image
 
-# Open the image (ensure that the WebP format is supported by Pillow)
 img = Image.open('image1.png')
 
-# Specify the correct path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'  # Correct path to Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 l=[]
 g=[]
-# Extract text from the image
+
 text = pytesseract.image_to_string(img)
 for i in text:
 	l.append(i)
